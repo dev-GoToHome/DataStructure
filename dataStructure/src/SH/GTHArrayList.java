bcde5b4e7f7cd2a79ce96bf73c1c219fbf954b80
mojoblue7 <lsh18542@gmail.com>
2020-08-08 16:30:40 +0900

    feat: toString 메소드 추가, setDefault 메소드 추가

--- dataStructure/src/SH/GTHArrayList.java
+++ dataStructure/src/SH/GTHArrayList.java
@@ -1,48 +1,54 @@
-package skeleton;
+package SH;
 
 // 이 ArrayList는 Integer Type만 다룬다
 public class GTHArrayList {
-    private int size;     // ArrayList 초기 사이즈 설정 변수
+    private static final int DEFAULT_SIZE = 10;   // ArrayList 초기 사이즈 설정 변수
+    private int size ;     // ArrayList 초기 사이즈 설정 변수
     private int index;     // ArrayList 초기 포인터 위치 변수
     private int array[];     // 배열 선언용 변수
 
     // 클래스 생성자
-    GTHArrayList() {
+    GTHArrayList(){
         // 1. 배열 선언
-        // 2. 클래스의 size 초기 세팅 진행 => size 10으로
+        setDefault(DEFAULT_SIZE);
     }
 
     // 클래스 생성자 Overiding
-    GTHArrayList(int size) {
-        // 1. 배열 선언
-
+    GTHArrayList(int size){
+        setDefault(size);
     }
 
-    public void add(int data) {
+    private void setDefault(int size){
+        this.size = size;
+        this.array = new int[this.size];
+    }
+
+    public void add(int data){
         // 3. add method 구현
         // 현재 포인터의 위치에 데이터를 추가
         // 데이터 추가 후 인덱스+1
+
     }
 
-    public void remove(int index) {
+    public void remove(int index){
         // 4. remove method 구현
         // 해당 인덱스의 데이터를 제거
         // 중간 데이터를 제거한 경우 뒤에 있는 데이터를 앞으로 밀어주어야 함.
     }
 
-    public int indexOf(int data) {
+    public int indexOf(int data){
         // 5. indexOf method 구현
         // 해당 데이터가 있는 위치의 인덱스를 반환
         return 0;
     }
 
-    public int size() {
+    public int size(){
         // 6. size method 구현
         // ArrayList의 크기를 반환
-        return this.size;
+        return 0;
     }
 
-    public boolean contains(int data) {
+    public boolean contains(int data){
         // 7. 해당 데이터가 ArrayList 내부에 존재 여부를 반환
         // 있으면 true, 없으면 false
         return false;
@@ -62,7 +68,7 @@ public class GTHArrayList {
     }
 
     // 클래스를 테스트 해 볼 수 있는 메인 함수
-    public static void main(String[] args){
+    public static void main(String args[]){
         GTHArrayList aryList = new GTHArrayList();
         System.out.println("데이터 출력");
         System.out.println(aryList.toString());
