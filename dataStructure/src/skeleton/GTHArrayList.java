e62f7482f1096876407da498b1aaf8fe68998efc
mojoblue7 <lsh18542@gmail.com>
2020-08-08 15:55:13 +0900

    chore: 정수용 ArrayList로 변경

--- dataStructure/src/skeleton/GTHArrayList.java
+++ dataStructure/src/skeleton/GTHArrayList.java
@@ -1,15 +1,14 @@
 package skeleton;
 
-// <T>는 배열에 들어갈 타입을 설정한다.
-// Integer, String 등의 타입이 들어간다.
-public class GTHArrayList<T> {
+// 이 ArrayList는 Integer Type만 다룬다
+public class GTHArrayList{
     private int size ;     // ArrayList 초기 사이즈 설정 변수
     private int index;     // ArrayList 초기 포인터 위치 변수
     private int array;     // 배열 선언용 변수
 
     // 클래스 생성자
     GTHArrayList(){
-        // 1. 배열 선언
+        array =;
     }
 
     // 클래스 생성자 Overiding
@@ -19,7 +18,7 @@ public class GTHArrayList<T> {
 
     }
 
-    public void add(T data){
+    public void add(int data){
         // 3. add method 구현
         // 현재 포인터의 위치에 데이터를 추가
         // 데이터 추가 후 인덱스+1
@@ -31,7 +30,7 @@ public class GTHArrayList<T> {
         // 중간 데이터를 제거한 경우 뒤에 있는 데이터를 앞으로 밀어주어야 함.
     }
 
-    public int indexOf(T data){
+    public int indexOf(int data){
         // 5. indexOf method 구현
         // 해당 데이터가 있는 위치의 인덱스를 반환
         return 0;
@@ -43,7 +42,7 @@ public class GTHArrayList<T> {
         return 0;
     }
 
-    public boolean contains(T data){
+    public boolean contains(int data){
         // 7. 해당 데이터가 ArrayList 내부에 존재 여부를 반환
         // 있으면 true, 없으면 false
         return false;
@@ -51,7 +50,7 @@ public class GTHArrayList<T> {
 
     // 클래스를 테스트 해 볼 수 있는 메인 함수
     public static void main(String args[]){
-        GTHArrayList<Integer> aryList = new GTHArrayList<Integer>();
+        GTHArrayList aryList = new GTHArrayList();
         System.out.println("데이터 출력");
     }
 }
